fe0b9dd4c7e79bec9987976fd16b82671c401635
NehamaMandelbaum <nehama1836@gmail.com>
2022-05-16 14:17:59 -0300

    fix: fixing project setup

--- src/data-source.ts
+++ src/data-source.ts
@@ -12,7 +12,7 @@ export const AppDataSource =
     : new DataSource({
         type: "postgres",
         host: "localhost",
-        port: 45432,
+        port: 5432,
         username: process.env.POSTGRES_USER,
         password: process.env.POSTGRES_PASSWORD,
         database: process.env.POSTGRES_DB,
