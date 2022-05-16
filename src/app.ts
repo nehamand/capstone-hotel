a4b11e4822c7213b6687831533e788a078a34465
henriquemarioto <henrique.marioto@outlook.com>
2022-05-16 18:22:05 -0300

    feat: app error

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,15 @@
+import "express-async-errors"
 import express from "express"
 import "reflect-metadata"
 import routes from "./routes"
+import errorHandling from "./middlewares/errorHandling.middleware"
 
 const app = express()
 
 app.use(express.json())
 
 app.use(routes)
+
+app.use(errorHandling)
 
 export default app
---
+++ src/errors/AppError.ts
@@ -0,0 +1,11 @@
+import { number } from "yup";
+
+export default class AppError {
+  statusCode: number;
+  message: string;
+
+  constructor(message: string, statusCode = 400) {
+    this.message = message;
+    this.statusCode = statusCode;
+  }
+}
