02b6972fd229cad61e1f86a77e32bdf0e0fb4a2e
Luiz Felipe Dias <lufelipe.dias@gmail.com>
2022-05-17 13:03:47 -0300

    Feat: add employeeRouter

--- src/app.ts
+++ src/app.ts
@@ -3,12 +3,14 @@ import express from "express"
 import "reflect-metadata"
 import routes from "./routes"
 import errorHandling from "./middlewares/errorHandling.middleware"
+import employeeRouter from "./routes/employee.routes"
 
 const app = express()
 
 app.use(express.json())
 app.use(errorHandling)
 
+app.use("/employees", employeeRouter)
 app.use(routes)
 
 export default app
