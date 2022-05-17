48aa9addba9cdd3c25ea49772e178bf661d40a45
NehamaMandelbaum <nehama1836@gmail.com>
2022-05-17 13:39:56 -0300

    fix: minor adjustments in services crud

--- src/controllers/service.controller.ts
+++ src/controllers/service.controller.ts
@@ -1,56 +1,56 @@
-import { Request, Response } from "express";
-import changeStatusService from "../services/service/changeStatus.service";
-import createService from "../services/service/createService.service";
-import getAllServices from "../services/service/getAllServices.service";
-import getOneService from "../services/service/getOneService.service";
-import updateService from "../services/service/updateService.service";
+import {Request, Response} from "express"
+import changeStatusService from "../services/service/changeStatus.service"
+import createService from "../services/service/createService.service"
+import getAllServices from "../services/service/getAllServices.service"
+import getOneService from "../services/service/getOneService.service"
+import updateService from "../services/service/updateService.service"
 
 class ServiceController {
   static async create(req: Request, res: Response) {
-    const { name, description, price } = req.body;
+    const {name, description, price} = req.body
 
-    const newService = await createService({ name, description, price });
+    const newService = await createService({name, description, price})
 
-    return res.status(201).json(newService);
+    return res.status(201).json(newService)
   }
 
   static async index(req: Request, res: Response) {
-    const status = req.query.status;
+    const status = req.query.status || "true"
 
-    const services = await getAllServices(status as string);
+    const services = await getAllServices(status as string)
 
-    return res.status(200).json(services);
+    return res.status(200).json(services)
   }
 
   static async show(req: Request, res: Response) {
-    const { id } = req.params;
+    const {id} = req.params
 
-    const service = await getOneService(id);
+    const service = await getOneService(id)
 
-    return res.status(200).json(service);
+    return res.status(200).json(service)
   }
 
   static async update(req: Request, res: Response) {
-    const { id } = req.params;
-    const { name, price, description, status } = req.body;
+    const {id} = req.params
+    const {name, price, description, status} = req.body
 
     const updatedService = await updateService(id, {
       name,
       price,
       description,
-      status
-    });
+      status,
+    })
 
-    return res.status(200).json(updatedService);
+    return res.status(200).json(updatedService)
   }
 
   static async delete(req: Request, res: Response) {
-    const { id } = req.params;
+    const {id} = req.params
 
-    const changeService = await changeStatusService(id);
+    const changeService = await changeStatusService(id)
 
-    return res.status(200).json(changeService);
+    return res.status(200).json(changeService)
   }
 }
 
-export default ServiceController;
+export default ServiceController
