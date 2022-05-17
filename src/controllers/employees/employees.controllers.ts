005166bd0a3da7aa71db85fba0ba61fd43d65b61
Luiz Felipe Dias <lufelipe.dias@gmail.com>
2022-05-17 13:04:12 -0300

    Feat: create controllers for employees

--- src/controllers/employees/employees.controllers.ts
+++ src/controllers/employees/employees.controllers.ts
@@ -1,6 +1,8 @@
 import { Request, Response } from "express"
 
 import createEmployeeService from "../../services/employees/createEmployee.service"
+import getAllEmployees from "../../services/employees/getAllEmployees.service"
+import getOneEmployeeService from "../../services/employees/getOneEmployee.service"
 
 class employeeControllers {
   static async store(req: Request, res: Response) {
@@ -14,10 +16,20 @@ class employeeControllers {
       status,
     })
 
-    return newUser
+    return res.status(201).json(newUser)
   }
-  static async index(req: Request, res: Response) {}
-  static async show(req: Request, res: Response) {}
+  static async index(req: Request, res: Response) {
+    const employees = await getAllEmployees()
+
+    return res.status(200).json(employees)
+  }
+  static async show(req: Request, res: Response) {
+    const { id } = req.params
+
+    const employee = await getOneEmployeeService(id)
+
+    return res.status(200).json(employee)
+  }
   static async update(req: Request, res: Response) {}
   static async delete(req: Request, res: Response) {}
 }
