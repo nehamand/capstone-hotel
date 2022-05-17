de900410223fc9c3bea1bcccb5517f10401be004
bryantorresribeiro <bryant0rr3sribeiro@gmail.com>
2022-05-17 12:57:48 -0500

    fix: correcting structure

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,6 @@
 import "express-async-errors"
 import express from "express"
 import "reflect-metadata"
-import routes from "./routes"
 import errorHandling from "./middlewares/errorHandling.middleware"
 import serviceRouter from "./routes/service.routes"
 import clientRouter from "./routes/client.routes"
@@ -12,9 +11,6 @@ app.use(express.json())
 
 app.use("/service", serviceRouter)
 app.use("/clients", clientRouter)
-
 app.use(errorHandling)
-
-// app.use(routes)
 
 export default app
