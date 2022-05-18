89c52fc0e827dcea8f830ab55d49f9dc745ece77
NehamaMandelbaum <nehama1836@gmail.com>
2022-05-18 13:59:04 -0300

    feat: middleware to check if user is admin

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -5,10 +5,11 @@ import ensureAuth from "../middlewares/ensureAuth.middleware"
 import serviceRouter from "./service.routes"
 import employeeRouter from "./employee.routes"
 import sessionRouter from "./session.routes"
+import isAdminMiddleware from "../middlewares/isAdmin.middleware"
 
 const routes = Router()
 
-routes.use("/services", ensureAuth, serviceRouter)
+routes.use("/services", ensureAuth, isAdminMiddleware, serviceRouter)
 routes.use("/employees", employeeRouter)
 routes.use("/sessions", sessionRouter)
 
