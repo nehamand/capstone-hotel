9d98fa15c7ec6687fb97b71c42d6d4313ffd08fd
NehamaMandelbaum <nehama1836@gmail.com>
2022-05-18 12:01:46 -0300

    fix: migration fixing email column in client model

--- src/controllers/clients.controllers.ts
+++ src/controllers/clients.controllers.ts
@@ -1,36 +1,42 @@
-import { Request, Response } from "express";
+import {Request, Response} from "express"
 
-import createClient from "../services/clients/clientsCreate.service";
-import listClient from "../services/clients/clientsList.service";
-import listOneClient from "../services/clients/clientsListOne.service";
-import updateClient from "../services/clients/clientsUpdate.service";
-import deleteClient from "../services/clients/clientsDelete.service";
+import createClient from "../services/clients/clientsCreate.service"
+import listClient from "../services/clients/clientsList.service"
+import listOneClient from "../services/clients/clientsListOne.service"
+import updateClient from "../services/clients/clientsUpdate.service"
+import deleteClient from "../services/clients/clientsDelete.service"
 
-export default class ClientsController{
-    static store = async (req: Request, res: Response)=>{
-        const {name, birthDate, cpf, cellphone, bedroom_id, created_by} = req.body;
-        const newClient = await createClient({name, birthDate, cpf, cellphone, bedroom_id, created_by});
-        return res.status(201).json(newClient);
-    };
-    static index = async (req: Request, res: Response)=>{
-        const {id} = req.params;
-        const client = await listOneClient(id);
-        return res.status(200).json(client);
-    };
-    static show = async (req: Request, res: Response)=>{
-        const status = req.query.status || "true"
-        const client = await listClient(status as string);
-        return res.status(200).json(client);
-    };
-    static update = async (req: Request, res: Response)=>{
-        const {id} = req.params;
-        const {name, birthDate, cpf, cellphone} = req.body;
-        const client = await updateClient(id,{name, birthDate, cpf, cellphone});
-        return res.status(201).json(client);
-    };
-    static delete = async (req: Request, res: Response)=>{
-        const {id} = req.params;
-        const client = await deleteClient(id);
-        return res.status(200).json(client);
-    };
+export default class ClientsController {
+  static store = async (req: Request, res: Response) => {
+    const {name, birthDate, cpf, cellphone, bedroom_id} = req.body
+    const newClient = await createClient({
+      name,
+      birthDate,
+      cpf,
+      cellphone,
+      bedroom_id,
+    })
+    return res.status(201).json(newClient)
+  }
+  static index = async (req: Request, res: Response) => {
+    const {id} = req.params
+    const client = await listOneClient(id)
+    return res.status(200).json(client)
+  }
+  static show = async (req: Request, res: Response) => {
+    const status = req.query.status || "true"
+    const client = await listClient(status as string)
+    return res.status(200).json(client)
+  }
+  static update = async (req: Request, res: Response) => {
+    const {id} = req.params
+    const {name, birthDate, cpf, cellphone} = req.body
+    const client = await updateClient(id, {name, birthDate, cpf, cellphone})
+    return res.status(201).json(client)
+  }
+  static delete = async (req: Request, res: Response) => {
+    const {id} = req.params
+    const client = await deleteClient(id)
+    return res.status(200).json(client)
+  }
 }
--- src/services/clients/clientsCreate.service.ts
+++ src/services/clients/clientsCreate.service.ts
@@ -1,28 +1,26 @@
-import { AppDataSource } from "../../data-source";
-import Client from "../../models/Clients";
+import {AppDataSource} from "../../data-source"
+import Client from "../../models/Clients"
 
 interface ClientProps {
-    name: string;
-    birthDate: Date;
-    cpf: string;
-    cellphone: string;
-    bedroom_id: Client;
-    created_by?: string;
+  name: string
+  birthDate: Date
+  cpf: string
+  cellphone: string
+  bedroom_id: Client
 }
 
-const createClient = async (data:ClientProps) =>{
+const createClient = async (data: ClientProps) => {
+  const clientRepository = AppDataSource.getRepository(Client)
 
-  const clientRepository = AppDataSource.getRepository(Client);
-
-  const newClient = new Client();
-  newClient.name = data.name;
-  newClient.birthDate = data.birthDate;
-  newClient.cpf = data.cpf;
-  newClient.cellphone = data.cellphone;
-  newClient.bedroom = data.bedroom_id;
-  clientRepository.create(newClient);
-  await clientRepository.save(newClient);
-  return newClient;
+  const newClient = new Client()
+  newClient.name = data.name
+  newClient.birthDate = data.birthDate
+  newClient.cpf = data.cpf
+  newClient.cellphone = data.cellphone
+  newClient.bedroom = data.bedroom_id
+  clientRepository.create(newClient)
+  await clientRepository.save(newClient)
+  return newClient
 }
 
-export default createClient;
+export default createClient
