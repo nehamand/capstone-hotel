63ed01764b82e84c7adaeeaaa42036a43c497f30
NehamaMandelbaum <nehama1836@gmail.com>
2022-05-17 22:32:48 -0300

    feat: merging adjustments in employee crud

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,5 +1,10 @@
 import Router from "express"
+import serviceRouter from "./service.routes"
+import employeeRouter from "./employee.routes"
 
 const routes = Router()
+
+routes.use("/service", serviceRouter)
+routes.use("/employees", employeeRouter)
 
 export default routes
