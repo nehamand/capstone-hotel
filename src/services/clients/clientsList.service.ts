56bf9d42371980edf01dd15811ec60309db32dfa
henriquemarioto <henrique.marioto@outlook.com>
2022-05-19 15:19:51 -0300

    feat: format client data on get

--- src/services/clients/clientsList.service.ts
+++ src/services/clients/clientsList.service.ts
@@ -1,11 +1,17 @@
 import { AppDataSource } from "../../data-source";
 import Client from "../../models/Clients";
+import formatGetClientData from "../../utils/formatGetClientData";
 
 const listClient = async (status:string) =>{
     const clientRepository = AppDataSource.getRepository(Client);
+    
     const clients = await clientRepository.find();
+
     const statusClients  = clients.filter(client => client.status.toString() === status)
-    return statusClients;
+
+    const fomatedClients = statusClients.map(client => formatGetClientData({client}))
+
+    return fomatedClients;
 } 
 
 export default listClient;
