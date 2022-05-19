af4374dba4e417206fad19ba2431d59a9448be30
NehamaMandelbaum <nehama1836@gmail.com>
2022-05-19 12:03:46 -0300

    fix: fixing middlewares

--- src/routes/service.routes.ts
+++ src/routes/service.routes.ts
@@ -1,13 +1,13 @@
-import { Router } from "express";
-import ServiceController from "../controllers/service.controller";
+import {Router} from "express"
+import ServiceController from "../controllers/service.controller"
+import isAdminMiddleware from "../middlewares/isAdmin.middleware"
 
 const serviceRouter = Router()
 
-
-serviceRouter.post('', ServiceController.create)
-serviceRouter.get('', ServiceController.index)
-serviceRouter.get('/:id', ServiceController.show)
-serviceRouter.patch('/:id', ServiceController.update)
-serviceRouter.delete('/:id', ServiceController.delete)
+serviceRouter.post("", isAdminMiddleware, ServiceController.create)
+serviceRouter.get("", ServiceController.index)
+serviceRouter.get("/:id", ServiceController.show)
+serviceRouter.patch("/:id", isAdminMiddleware, ServiceController.update)
+serviceRouter.delete("/:id", isAdminMiddleware, ServiceController.delete)
 
 export default serviceRouter
