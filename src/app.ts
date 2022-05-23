3b32b4ac48b2a4e293d164537afe4d07c1180d30
henriquemarioto <henrique.marioto@outlook.com>
2022-05-23 13:06:16 -0300

    add: cors

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,14 @@
 import "express-async-errors"
 import "reflect-metadata"
 
+import cors from "cors"
 import errorHandling from "./middlewares/errorHandling.middleware"
 import express from "express"
 import routes from "./routes"
 
 const app = express()
 
+app.use(cors());
 app.use(express.json())
 
 app.use(routes)
