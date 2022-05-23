3a91bbe12c9f507f8176a311fe8951e20755f8c6
NehamaMandelbaum <nehama1836@gmail.com>
2022-05-23 10:47:45 -0300

    fix: fixing yup validation for cellphone and cpf- digits only and length 11

--- src/routes/employee.routes.ts
+++ src/routes/employee.routes.ts
@@ -5,6 +5,7 @@ import ensureAuth from "../middlewares/ensureAuth.middleware"
 import {expressYupMiddleware} from "express-yup-middleware"
 import createEmployeeSchema from "../validations/employees/createEmployee.validation"
 import updateEmployeeSchema from "../validations/employees/updateEmployee.validation"
+import isAdminMiddleware from "../middlewares/isAdmin.middleware"
 
 const employeeRouter = Router()
 
@@ -18,9 +19,15 @@ employeeRouter.post(
 employeeRouter.patch(
   "/:id",
   expressYupMiddleware({schemaValidator: updateEmployeeSchema}),
+  isAdminMiddleware,
   ensureAuth,
   employeeControllers.update
 )
-employeeRouter.delete("/:id", ensureAuth, employeeControllers.delete)
+employeeRouter.delete(
+  "/:id",
+  ensureAuth,
+  isAdminMiddleware,
+  employeeControllers.delete
+)
 
 export default employeeRouter
--- src/validations/employees/createEmployee.validation.ts
+++ src/validations/employees/createEmployee.validation.ts
@@ -5,7 +5,11 @@ const createEmployeeSchema = {
     body: {
       yupSchema: yup.object().shape({
         name: yup.string().required("Name is Required"),
-        cpf: yup.string().required("CPF is required!"),
+        cpf: yup
+          .string()
+          .required("CPF is required!")
+          .length(11, "Cpf must have 11 digits")
+          .matches(/^\d+$/, "The field should have digits only"),
         password: yup.string().required("Password is required!"),
         admin: yup.boolean(),
         status: yup.boolean(),
